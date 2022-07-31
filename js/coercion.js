5965fa19d7a86f9df0ff4fd3903c13f4f6fba81f
SOunit <workbookwork7@gmail.com>
2022-07-31 16:30:22 -0700

    add memo

---
+++ js/coercion.js
@@ -0,0 +1,18 @@
+const total = 2 + 3 + "7";
+console.log(2 + 3);
+console.log(total);
+console.log(typeof total);
+
+console.log(typeof String("aaa"));
+console.log(typeof Boolean(true));
+console.log(typeof Number("10"));
+
+//
+//
+//
+//
+//
+//
+//
+// 1. implicit coercion
+// 2. explicit coercion
