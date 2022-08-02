ae126495f2aec78c3fcb75f234cc69a000e0b6d8
SOunit <workbookwork7@gmail.com>
2022-08-02 13:58:32 -0700

    add sample

---
+++ js/create-array.js
@@ -0,0 +1,7 @@
+const example1 = [1];
+const badExample = new Array();
+const example2 = example1.map((val) => val + 1);
+
+console.log(example1);
+console.log(example2);
+console.log(badExample);
---
+++ js/undefined-null.js
@@ -0,0 +1,22 @@
+const user1 = {};
+
+let user2 = null;
+
+console.log(user1);
+console.log(user1.firstName);
+console.log(user2);
+console.log(user3);
+
+//
+//
+//
+//
+//
+// undefined
+// - not assigned value
+
+// null
+// - assigned value
+
+// undeclared
+// not exist
