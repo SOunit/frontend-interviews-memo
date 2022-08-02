be64c87ff136d4c85f8e8b8473b3c240504a68d3
SOunit <workbookwork7@gmail.com>
2022-08-02 16:50:08 -0700

    add memo

---
+++ js/nan.js
@@ -0,0 +1,22 @@
+console.log(0 / 0);
+console.log("" / 0);
+console.log(parseInt("hello"));
+
+console.log(typeof NaN);
+console.log(isNaN(3));
+console.log(isNaN(NaN));
+
+//
+//
+//
+//
+//
+//
+//
+// how to get NaN
+// 1. calc on string / etc.
+// 2. parse to number
+//
+// how to check NaN
+// 1. typeof NaN = number
+// 2. isNaN can check correctly
---
+++ js/undeclared-undefined.js
@@ -0,0 +1,7 @@
+const user = {};
+let example;
+
+console.log(user.firstName);
+console.log(example);
+
+console.log(example2);
