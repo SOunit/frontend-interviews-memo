5e06def476fb9116d20426a73bd68c543266641d
SOunit <workbookwork7@gmail.com>
2022-08-02 17:06:08 -0700

    add sample

---
+++ js/dom-selector.js
@@ -0,0 +1,21 @@
+console.log(document.getElementById("id-example"));
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByClassName("some-class"));
+console.log(document.querySelector("body"));
+console.log(document.querySelector("div"));
+console.log(document.querySelector("h1"));
+console.log(document.querySelector("h1.some-class"));
+console.log(document.querySelectorAll("h1"));
+
+//
+//
+//
+//
+//
+//
+//
+//
+// 1. getElementById
+// 2. getElementByTagName
+// 3. getElementById
+// 4. getElementById
